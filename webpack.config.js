d514e3968cb9ceff5b84e2f253394d3288536184
Joseph Sharratt <joseph.sharratt@gmail.com>
2021-05-12 18:06:31 +0100

    testing a possibly deployment fix 3

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const env = process.env.NODE_ENV || 'development'
 module.exports = {
   mode: env,
   optimization: { minimize: env === 'production' },
-  entry: `${__dirname}`,
+  entry: `/app/client/index.js`,
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
